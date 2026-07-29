7d60b3afe1b522ef9d827c4db332a315f5daa32d
pcus8112 <pcus81@gmail.com>
2026-07-29 08:00:35 +0200

    Add files via upload

---
+++ js/fractions.js
@@ -0,0 +1,23 @@
+/*
+THE MATH FINAL GIRL
+TEST FILE
+
+This file intentionally contains JavaScript even though
+its filename ends with .html.
+
+Rename this file to:
+
+fractions.js
+
+and upload it into:
+
+js/fractions.js
+*/
+
+"use strict";
+
+console.log("Final Girl JavaScript test successful!");
+
+function helloFinalGirl() {
+    return "Hello Final Girl!";
+}
